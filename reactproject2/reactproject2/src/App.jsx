161d4a9152f179d805ad7c194e77d66f56b00365
Arun Prakash <57718711+arunpts@users.noreply.github.com>
2024-07-10 12:43:11 +0530

    Header and footer

--- reactproject2/reactproject2/src/App.jsx
+++ reactproject2/reactproject2/src/App.jsx
@@ -1,10 +1,14 @@
 
+import Header from "./Header.jsx"
 import Food from "./Food.jsx"
+import Footer from "./Footer.jsx"
 function App() {
 
     return (
         <>
+            <Header />
             <Food />
+            <Footer />
         </>
     );
 }
